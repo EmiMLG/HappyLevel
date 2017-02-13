df81bc2786fd9c8c3b25f4f45932f1cea83949dc
Emi <emimlg1@gmail.com>
2017-02-13 11:14:23 +0100

    added button feel

--- app/src/main/java/es/telefonica/talentum/myapplication/MainActivity.java
+++ app/src/main/java/es/telefonica/talentum/myapplication/MainActivity.java
@@ -1,13 +1,50 @@
 package es.telefonica.talentum.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_main___text_view)TextView textViewWelcome;
+    @BindView(R.id.activity_main___button_happy)ImageView buttonHappy;
+    @BindView(R.id.activity_main___button_normal)ImageView buttonNormal;
+    @BindView(R.id.activity_main___button_sad)ImageView buttonSad;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        buttonHappy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonNormal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonSad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
